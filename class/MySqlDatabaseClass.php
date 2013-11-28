c40717fe6644ca9a0e9543afbd89c62f0f2ebbd7
Arjan de Ruijter <rra@mboutrecht.nl>
2013-11-28 16:01:03 +0100

    Logboek ingevuld en MySqlDatabase.php gemaakt

---
+++ class/MySqlDatabaseClass.php
@@ -0,0 +1,17 @@
+<?php
+	require_once('config/config.php');
+	
+	//Dit is de class definitie van de MySqlDatabaseClass
+	class MySqlDatabaseClass
+	{
+		
+		
+		//Constructor in PHP heeft altijd dezelfde naam __construct
+		public function __construct()
+		{
+			
+			
+		}
+		
+	}
+?>
